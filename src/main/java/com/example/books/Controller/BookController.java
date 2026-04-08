29c4a430475dc900ed905aa3dafb92c645765d6f
Simon Vargas <sjvargas01@gmail.com>
2026-04-08 01:16:38 -0400

    renamed get api

--- src/main/java/com/example/books/Controller/BookController.java
+++ src/main/java/com/example/books/Controller/BookController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class BookController {
 
-    @GetMapping("/api-endpoint")
+    @GetMapping("/api")
     public String firstApi() {
         return "Hello World";
     }
