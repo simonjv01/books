0273d102183340ca650576ddd7aaf6320f6ee451
Simon Vargas <sjvargas01@gmail.com>
2026-04-11 22:06:50 -0400

    created list of books for book controller

--- src/main/java/com/example/books/Controller/BookController.java
+++ src/main/java/com/example/books/Controller/BookController.java
@@ -1,9 +1,27 @@
 package com.example.books.Controller;
 
+import com.example.books.entity.Book;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class BookController {
+
+    private  final List<Book> books = new ArrayList<>();
+    
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title one", "Author One", "science"),
+                new Book("Title two", "Author Two", "fiction"),
+                new Book("Title three", "Author Three", "history"),
+                new Book("Title four", "Author Four", "science"),
+                new Book("Title five", "Author Five", "fiction")
+
+        ));
+    }
 
     @GetMapping("/api/books")
     public List<Book> getBooks() {
