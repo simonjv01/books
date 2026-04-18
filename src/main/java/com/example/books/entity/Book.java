541418e23d020fdb6907bd27c148102a5ecf35fe
Simon Vargas <sjvargas01@gmail.com>
2026-04-18 19:38:49 -0400

    added long id and int rating as parameters

--- src/main/java/com/example/books/entity/Book.java
+++ src/main/java/com/example/books/entity/Book.java
@@ -2,14 +2,18 @@ package com.example.books.entity;
 
 public class Book {
 
+    private long id;
     private String title;
     private String author;
     private String category;
+    private int rating;
 
-    public Book(String title, String author, String category) {
+    public Book(long id, String title, String author, String category, int rating) {
+        this.id = id;
         this.title = title;
         this.author = author;
         this.category = category;
+        this.rating = rating;
     }
 
     public String getTitle() {
