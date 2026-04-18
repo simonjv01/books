8bacddb5095f77143a9874fd66b564af15edd55e
Simon Vargas <sjvargas01@gmail.com>
2026-04-18 19:59:28 -0400

    refactored apis, changed from title to id as paramater

--- src/main/java/com/example/books/Controller/BookController.java
+++ src/main/java/com/example/books/Controller/BookController.java
@@ -41,10 +41,10 @@ public class BookController {
                 .toList();
     }
 
-    @GetMapping("/{title}")
-    public Book getBookByTitle(@PathVariable String title) {
+    @GetMapping("/{id}")
+    public Book getBookById(@PathVariable long id) {
         return books.stream()
-                .filter(book -> book.getTitle().equalsIgnoreCase(title))
+                .filter(book -> book.getId() == id)
                 .findFirst()
                 .orElse(null);
         }
@@ -61,10 +61,10 @@ public class BookController {
             
         }
 
-    @PutMapping("/{title}")
-    public void updateBook(@PathVariable String title, @RequestBody Book updatedBook) {
+    @PutMapping("/{id}")
+    public void updateBook(@PathVariable long id, @RequestBody Book updatedBook) {
         for (int i=0; i < books.size(); i++) {
-            if (books.get(i).getTitle().equalsIgnoreCase(title)) {
+            if (books.get(i).getId() == id) {
                 books.set(i, updatedBook);
                 return;
             }
@@ -72,9 +72,9 @@ public class BookController {
         
     }
 
-    @DeleteMapping("/{title}")
-    public void deleteBook(@PathVariable String title) {
-        books.removeIf(book -> book.getTitle().equalsIgnoreCase(title));
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable long id) {
+        books.removeIf(book -> book.getId() == id);
 
     }
         
