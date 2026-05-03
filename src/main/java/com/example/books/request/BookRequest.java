62b054c2eba5404756930f8380eb2892a1b99ae1
Simon Vargas <sjvargas01@gmail.com>
2026-05-03 12:23:23 -0400

    added message to constraints for rating

--- src/main/java/com/example/books/request/BookRequest.java
+++ src/main/java/com/example/books/request/BookRequest.java
@@ -9,8 +9,8 @@ public class BookRequest {
     private String author;
     private String category;
 
-    @Min(value = 1)
-    @Max(value = 5)
+    @Min(value = 1, message = "Rating must be at least 1" )
+    @Max(value = 5, message = "Rating must be at most 5")
     private int rating;
 
 
